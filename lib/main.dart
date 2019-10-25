1e1ba11297b3ec32d43c00b16f8187766ea580e4
Kristina Capistrano <kristina.gomezcapistrano@gmail.com>
2019-10-24 23:23:10 -0700

    US18TASK4 Adjusted display logo

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  // This widget is the roost of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
@@ -26,52 +27,74 @@ class LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       backgroundColor: Color.fromRGBO(246, 59, 73, 10),
-      body: new Stack(
+      body: new Stack (
         children: <Widget>[
-          new Image(
-            image: new AssetImage("assets/PCH_LOGO.jpg"),
-            fit:  BoxFit.cover,
-            color: Colors.red,
-            colorBlendMode: BlendMode.dst
+          Positioned(
+            right: 70.0,
+            top: 70.0,
+            child: new Image(
+
+                image: new AssetImage("assets/resizedLOGO.jpg"),
+                alignment: Alignment.topCenter,
+                fit:  BoxFit.none,
+                color: Colors.red,
+                colorBlendMode: BlendMode.dst//dst
+            ),
           ),
+
+
           new Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               new Form(
                 child: Theme(
                   data: new ThemeData(
-                    brightness: Brightness.light,primarySwatch: Colors.amber,
+                    brightness: Brightness.dark,primarySwatch: Colors.red,
                     inputDecorationTheme: new InputDecorationTheme(
                       labelStyle: new TextStyle(
-                        color: Colors.amber
+                        color: Colors.amber,
+                        fontSize: 20.0
                       )
                     )
                   ),
                   child: Container(
                     padding: const EdgeInsets.all(50.0),
+
                     child: new Column(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: <Widget>[
+
                         new TextFormField(
                           decoration: new InputDecoration(
-                            hintText: "Enter Email",
+                            labelText: "Enter Email",
                           ),
                           keyboardType: TextInputType.emailAddress,
                         ),
                         new TextFormField(
                           decoration: new InputDecoration(
-                            hintText: "Enter Password",
+                            labelText: "Enter Password",
                           ),
                           keyboardType: TextInputType.text,
                           obscureText: true,
-                        )
+                        ),
+                        new Padding(
+                            padding: const EdgeInsets.only(top: 20.0)
+                        ),
+                        new MaterialButton(
+                            color: Colors.amber,
+                            textColor: Colors.white,
+                            child: new Text("LOGIN"),
+
+                            onPressed: ()=>{}
+                            )
                       ],
                     ),
                   ),
                 ),
               )
             ],
-          )
+          ),
+
 
         ],
       ),
