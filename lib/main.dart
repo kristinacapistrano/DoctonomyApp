de744c7b2cd1b5608410666244047724dcaeb1d8
Kristina Capistrano <kristina.gomezcapistrano@gmail.com>
2019-10-24 19:28:09 -0700

    US18TASK2 Added logo and username/pw box

--- lib/main.dart
+++ lib/main.dart
@@ -21,17 +21,58 @@ class LoginPage extends StatefulWidget {
   State createState() => new LoginPageState();
 }
 
-class LoginPageState extends State<LoginPage>{
+class LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return new Scaffold(
-      backgroundColor: Color.fromRGBO(238, 48, 73, 10),
+      backgroundColor: Color.fromRGBO(246, 59, 73, 10),
       body: new Stack(
         children: <Widget>[
           new Image(
             image: new AssetImage("assets/PCH_LOGO.jpg"),
+            fit:  BoxFit.cover,
+            color: Colors.red,
+            colorBlendMode: BlendMode.dst
+          ),
+          new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new Form(
+                child: Theme(
+                  data: new ThemeData(
+                    brightness: Brightness.light,primarySwatch: Colors.amber,
+                    inputDecorationTheme: new InputDecorationTheme(
+                      labelStyle: new TextStyle(
+                        color: Colors.amber
+                      )
+                    )
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.all(50.0),
+                    child: new Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            hintText: "Enter Email",
+                          ),
+                          keyboardType: TextInputType.emailAddress,
+                        ),
+                        new TextFormField(
+                          decoration: new InputDecoration(
+                            hintText: "Enter Password",
+                          ),
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
           )
+
         ],
       ),
     );
