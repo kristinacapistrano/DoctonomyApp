eeb74114943561b8c8817bfb97e145e074f51684
psevertson <portersevertson@gmail.com>
2019-10-28 21:11:30 -0700

    Add login functionality with user models, loading state, and navigation

---
+++ lib/ui/screens/home.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import '../../models/state.dart';
+import '../../util/state_widget.dart';
+import './sign_in.dart';
+import '../../widgets/loading.dart';
+
+class HomeScreen extends StatefulWidget {
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  StateModel appState;
+  bool _loadingVisible = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget build(BuildContext context) {
+    appState = StateWidget.of(context).state;
+    if (!appState.isLoading &&
+        (appState.firebaseUserAuth == null ||
+            appState.user == null
+            // || appState.settings == null
+        )) {
+      return SignInScreen();
+    } else {
+      if (appState.isLoading) {
+        _loadingVisible = true;
+      } else {
+        _loadingVisible = false;
+      }
+      final logo = Hero(
+        tag: 'hero',
+        child: CircleAvatar(
+            backgroundColor: Colors.transparent,
+            radius: 60.0,
+            child: ClipOval(
+              child: Image.asset(
+                'assets/images/default.png',
+                fit: BoxFit.cover,
+                width: 120.0,
+                height: 120.0,
+              ),
+            )),
+      );
+
+      final signOutButton = Padding(
+        padding: EdgeInsets.symmetric(vertical: 16.0),
+        child: RaisedButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24),
+          ),
+          onPressed: () {
+            StateWidget.of(context).logOutUser();
+          },
+          padding: EdgeInsets.all(12),
+          color: Theme
+              .of(context)
+              .primaryColor,
+          child: Text('SIGN OUT', style: TextStyle(color: Colors.white)),
+        ),
+      );
+
+//check for null https://stackoverflow.com/questions/49775261/check-null-in-ternary-operation
+      final userId = appState?.firebaseUserAuth?.uid ?? '';
+      final email = appState?.firebaseUserAuth?.email ?? '';
+
+      final userIdLabel = Text('User Id: ');
+      final emailLabel = Text('Email: ');
+
+      return Scaffold(
+        backgroundColor: Colors.white,
+        body: LoadingScreen(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 48.0),
+              child: Center(
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      SizedBox(height: 48.0),
+                      userIdLabel,
+                      Text(userId,
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      SizedBox(height: 12.0),
+                      emailLabel,
+                      Text(email,
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      SizedBox(height: 12.0),
+                      signOutButton
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            inAsyncCall: _loadingVisible),
+      );
+    }
+  }
+}
