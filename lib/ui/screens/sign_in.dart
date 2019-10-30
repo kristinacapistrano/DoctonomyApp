07234c44a71fcaf85a4a5f3566acebdef640b155
Alex A <appdaddy@Alexs-MacBook-Pro.local>
2019-10-29 21:55:58 -0700

    Add efficient routing to main.dart

--- lib/ui/screens/sign_in.dart
+++ lib/ui/screens/sign_in.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
-//import 'package:flushbar/flushbar.dart';
 import 'package:flutter/services.dart';
 
-import '../../util/state_widget.dart';
 import '../../util/auth.dart';
+import '../../util/state_widget.dart';
 import '../../util/validator.dart';
-import '../../widgets/Hyperlink.dart';
 import '../../widgets/loading.dart';
 
 class SignInScreen extends StatefulWidget {
+  static const String id = 'sign_in_screen';
+
   _SignInScreenState createState() => _SignInScreenState();
 }
 
@@ -33,7 +33,7 @@ class _SignInScreenState extends State<SignInScreen> {
           fit: BoxFit.none,
           color: Colors.red,
           colorBlendMode: BlendMode.dst //dst
-      ),
+          ),
     );
 
     final email = TextFormField(
