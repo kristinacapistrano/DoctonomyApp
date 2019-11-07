eed22f8da4015870ad0470934b8cb97026a1174c
jkbulloc <jkbulloc@asu.edu>
2019-11-07 12:12:58 -0700

    replaced raised buttons with bottom app bar icons, still having trouble rendering image onto homepage

--- lib/views/homePage.dart
+++ lib/views/homePage.dart
@@ -24,34 +24,42 @@ class HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text("Home Page"),
       ),
-      //backgroundColor: Color.fromARGB(246, 59, 73, 10),
-      body: Column(
-        children: [
-          Text('Welcome to the Home Page!'),
-          // Positioned(
-          //   right: 70.0,
-          //   top: 70.0,
-          //   child: new Image(
-          //     image: new AssetImage("assets/resizedLOGO.jpg"),
-          //     alignment: Alignment.topCenter,
-          //     fit: BoxFit.none,
-          //     color: Colors.red,
-          //     colorBlendMode: BlendMode.dst,
-          //   ),
-          //)
-        RaisedButton(
-          child: Text('View Provider Information'),
-          onPressed: providerButtonTest,
-        ),
-        RaisedButton(
-          child: Text('Important Dates'),
-          onPressed: datesButtonTest,
+      bottomNavigationBar: BottomAppBar(
+        child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.home), 
+              onPressed: (){print('home icon pressed!');},
+              ),
+            IconButton(
+              icon: Icon(Icons.calendar_today),
+              onPressed: (){print('Calendar icon pressed!');},
+            ),
+            IconButton(
+              icon: Icon(Icons.notifications),
+              onPressed: (){print('Notifications Icon button Pressed!');},
+            ),
+          ],
           ),
-        RaisedButton(
-          child: Text('Reminders'),
-          onPressed: remindersButtonTest,
-          )
-        ],
+        ),
+      //backgroundColor: Color.fromARGB(246, 59, 73, 10),
+      body: new Column(
+        children: <Widget>[
+          //Text('Welcome to the Home Page!'),
+          Positioned(
+            right: 70.0,
+            top: 70.0,
+            child: new Image(
+              image: new AssetImage("assets/resizedLOGO.jpg"),
+              alignment: Alignment.topCenter,
+              fit: BoxFit.none,
+              color: Colors.red,
+              colorBlendMode: BlendMode.dst,
+            ),
+          ),
+        ], //children
       ),
     ); //scaffold is the root for all other elements to be place on the page.
   }
