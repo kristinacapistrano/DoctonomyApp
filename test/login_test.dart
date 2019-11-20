ad88c435725f1b88ffaa2d9fa55fbe3a3fe43fd5
psevertson <portersevertson@gmail.com>
2019-11-19 22:48:20 -0700

    Create test cases for login page (In Progress)

--- test/login_test.dart
+++ test/login_test.dart
@@ -1,33 +1,52 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility that Flutter provides. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:doctonomy_app/main.dart';
+import 'package:doctonomy_app/screens/sign_in.dart';
 
 void main() {
-  testWidgets('Login unit test', (WidgetTester tester) async {
-    //Write our login Unit Test here
-    /*
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+  testWidgets('Test Form Empty Validation', (WidgetTester tester) async {
+    //Tests that the form validation fails when textfields are empty
+    SignInScreen sis = new SignInScreen();
+    var app = new MediaQuery(data: new MediaQueryData(), child: new MaterialApp(home: sis));
+    await tester.pumpWidget(app);
+    Finder formWidgetFinder = find.byType(Form);
+    Form formWidget = tester.widget(formWidgetFinder) as Form;
+    GlobalKey<FormState> formKey = formWidget.key as GlobalKey<FormState>;
+    expect(formKey.currentState.validate(), isFalse);
+  });
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  testWidgets('Test Form Filled Validation', (WidgetTester tester) async {
+    //Sets up a test environment "app" on the SignInScreen widget
+    SignInScreen sis = new SignInScreen();
+    var app = new MediaQuery(data: new MediaQueryData(), child: new MaterialApp(home: sis));
+    await tester.pumpWidget(app);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    //Finds the email and password textfields by key
+    Finder email = find.byKey(new Key('email'));
+    Finder pwd = find.byKey(new Key('password'));
+
+    //Fills in the text on the email and password textfields
+    await tester.enterText(email, "test@email.com");
+    await tester.enterText(pwd, "test123");
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
-    */
+    //Finds the form widget
+    Finder formWidgetFinder = find.byType(Form);
+    Form formWidget = tester.widget(formWidgetFinder) as Form;
+    GlobalKey<FormState> formKey = formWidget.key as GlobalKey<FormState>;
+
+    //Asserts that the form is in a valid state
+    expect(formKey.currentState.validate(), isTrue);
+  });
+
+  testWidgets('Login unit test', (WidgetTester tester) async {
+    //Tests the login functionality with the test login account
+    SignInScreen sis = new SignInScreen();
+    var app = new MediaQuery(data: new MediaQueryData(), child: new MaterialApp(home: sis));
+    await tester.pumpWidget(app);
+    Finder email = find.byKey(new Key('email'));
+    Finder pwd = find.byKey(new Key('password'));
+
+    //TODO simulate login API, check for a successful response
   });
 }
