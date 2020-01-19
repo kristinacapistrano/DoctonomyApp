2ed992ff1eb04b6e9c31cd162c362d71ca38f62a
psevertson <portersevertson@gmail.com>
2020-01-18 18:33:57 -0700

    Remove unused Hero image

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -33,20 +33,7 @@ class _SignInScreenState extends State<SignInScreen> {
           fit: BoxFit.fitHeight,
           ),
     );
-
-    final pch_logo = Hero(
-      tag: 'hero',
-      child: new Image(
-        image: new AssetImage("assets/appbar_logo.png"),
-        alignment: Alignment.center,
-        fit: BoxFit.fitWidth,
-
-        //color: Colors.red,
-        //colorBlendMode: BlendMode.dst //dst
-      ),
-    );
-
-
+    
     final focus = FocusNode();
     final email = TextFormField(
       key: new Key('email'),
