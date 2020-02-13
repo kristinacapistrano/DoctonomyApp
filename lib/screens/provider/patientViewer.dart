92508fc49957bc73d11bf48de03f7a3fa0ea4695
psevertson <portersevertson@gmail.com>
2020-02-12 21:16:01 -0700

    View Patient's allergy list

--- lib/screens/provider/patientViewer.dart
+++ lib/screens/provider/patientViewer.dart
@@ -26,21 +26,13 @@ class _PatientViewerState extends State<PatientViewer> {
     super.initState();
   }
 
-  Future<List<dynamic>> getList() async {
+  Future<Map<String,dynamic>> getUserData() async {
     DocumentReference docRef = Firestore.instance.collection('users').document(userId ?? "");
     return docRef.get().then((datasnapshot) async {
       if (datasnapshot.exists) {
-        return datasnapshot.data['allergies'].toList();
-        /*List<dynamic> info = datasnapshot.data['patients'].toList();
-        List<dynamic> list = new List();
-        for(var uid in info) {
-          DocumentReference dr = Firestore.instance.collection('users').document(uid);
-          DocumentSnapshot ds = await dr.get();
-          list.add(ds);
-        }
-        return list;*/
+        return datasnapshot.data;
       } else {
-        return [];
+        return null;
       }
     });
   }
@@ -63,98 +55,102 @@ class _PatientViewerState extends State<PatientViewer> {
             iconTheme: IconThemeData(color: Colors.lightBlueAccent[700]),
             backgroundColor: Colors.white),
         body:
-          ListView(
-            children: <Widget>[
-              SizedBox(height: 20.0),
-              Text(' Upcoming Procedures'),
-              Card(child: ListTile(
-                leading: Icon(Icons.healing),
-                title: Text('Surgery'),
-                subtitle: Text('Coming up on 2/20/20'),
-                onTap: () {
-                  print("clicked Row");
-                },
-              )
-              ),
-              Card(child: ListTile(
-                leading: Icon(Icons.healing),
-                title: Text('Procedure #2'),
-                subtitle: Text('Coming up on 5/05/20'),
-                onTap: () {
-                  print("clicked Row");
-                },
-              )
-              ),
-              SizedBox(height: 20.0),
-              Text(' Reminders'),
-              Card(child: ListTile(
-                leading: Icon(Icons.alarm),
-                title: Text('Take medication'),
-                subtitle: Text('Every day at 5pm'),
-                onTap: () {
-                  print("clicked Row");
-                },
-              )
-              ),
-              SizedBox(height: 20.0),
-              Text(' Allergies'),
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 0, horizontal: 5),
+            child: FutureBuilder(
+              future: getUserData(),
+              builder: (context, AsyncSnapshot<Map<String,dynamic>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Center(child: CircularProgressIndicator());
+                } else {
+                  print(snapshot.data);
+  //                {firstName: Testname2, lastName: Testlastname, allergies: [peanuts], phone: (480) 123-4567}
+                    return Center(
+                      child: ListView(
+                        children: <Widget>[
+                          SizedBox(height: 20.0),
+                          Text('Upcoming Procedures', style: TextStyle(fontWeight: FontWeight.w500)),
+                          //TODO remove hardcoded card
+                          Card(child: ListTile(
+                            leading: Icon(Icons.healing),
+                            title: Text('Surgery'),
+                            subtitle: Text('Coming up on 2/20/20'),
+                            onTap: () {
+                              print("clicked Row");
+                            },
+                          )
+                          ),
 
-              FutureBuilder(
-                future: getList(),
-                builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Card(child: CircularProgressIndicator());
-                  } else {
-                    // TODO Improve showing as a list
-                    print(snapshot.data);
-                    if (snapshot.data.length > 0) {
-                      return Card(child: ListTile(
-                        title: Text(snapshot.data[0]),
-                        onTap: () {
-                          print("clicked Row");
-                        },
-                      )
-                      );
-                    } else {
-                      return Card(child: ListTile(
-                        title: Text("No Allergies"),
-                        onTap: () {
-                          print("clicked Row");
-                        },
-                      )
-                      );
-                    }
+                          SizedBox(height: 20.0),
+                          Text('Reminders', style: TextStyle(fontWeight: FontWeight.w500)),
+                          //TODO remove hardcoded card
+                          Card(child: ListTile(
+                            leading: Icon(Icons.alarm),
+                            title: Text('Take medication'),
+                            subtitle: Text('Every day at 5pm'),
+                            onTap: () {
+                              print("clicked Row");
+                            },
+                          )
+                          ),
 
-                  }
-              }),
+                          SizedBox(height: 20.0),
+                          Text('Allergies', style: TextStyle(fontWeight: FontWeight.w500)),
+                          Builder(builder: (BuildContext context) {
+                            var allergyList = snapshot.data["allergies"]?.toList() ?? [];
+                            if (allergyList.length > 0) {
+                              List<Widget> tiles = allergyList.fold(List<Widget>(), (total, el) {
+                                total.add(ListTile(title: Text(el, style: TextStyle(fontWeight: FontWeight.w500)), dense: true, onTap: () => {}));
+                                total.add(Divider(thickness: 1, indent: 10, endIndent: 10, height: 1));
+                                return total;
+                              });
+                              tiles.add(ListTile(title: Text("+ Add New"), dense: true, onTap: () => {}));
+                              return Card(child: Column(children: tiles.toList()));
+                            } else {
+                              return Card(child: ListTile(
+                                title: Text("No Allergies (Click here to add)"),
+                                onTap: () {
+                                  print("clicked Row");
+                                },
+                                dense: true
+                              )
+                              );
+                            }
+                          }),
 
-              SizedBox(height: 20.0),
-              Text(' Actions'),
-              Card(child: ListTile(
-                trailing: Icon(Icons.send),
-                title: Text('Send a message'),
-                onTap: () {
-                  print("clicked Row");
-                },
-              )
-              ),
-              Card(child: ListTile(
-                trailing: Icon(Icons.healing),
-                title: Text('Schedule new procedure'),
-                onTap: () {
-                  print("clicked Row");
-                },
-              )
-              ),
-              Card(child: ListTile(
-                trailing: Icon(Icons.clear),
-                title: Text('Remove from my list'),
-                onTap: () {
-                  print("clicked Row");
-                },
-              )
-              ),
-            ],
+                          SizedBox(height: 20.0),
+                          Text('Actions', style: TextStyle(fontWeight: FontWeight.w500)),
+                          Card(child: ListTile(
+                            trailing: Icon(Icons.send),
+                            title: Text('Send a message'),
+                            onTap: () {
+                              print("clicked Row");
+                            },
+                          )
+                          ),
+                          Card(child: ListTile(
+                            trailing: Icon(Icons.healing),
+                            title: Text('Schedule new procedure'),
+                            onTap: () {
+                              print("clicked Row");
+                            },
+                          )
+                          ),
+                          Card(child: ListTile(
+                            trailing: Icon(Icons.clear),
+                            title: Text('Remove from my list'),
+                            onTap: () {
+                              print("clicked Row");
+                            },
+                          )
+                          )
+                        ],
+                      ),
+                    );
+                }
+              }
+            )
+
           )
     );
   }
