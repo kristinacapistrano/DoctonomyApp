f90ef413bc3827d0e5ce763f661452c1bc552978
psevertson <portersevertson@gmail.com>
2020-02-25 22:42:23 -0700

    Add search bar to layout

--- lib/screens/provider/patientChooser.dart
+++ lib/screens/provider/patientChooser.dart
@@ -13,6 +13,7 @@ class PatientChooser extends StatefulWidget {
 class _PatientChooserState extends State<PatientChooser> {
   StateModel appState;
   List<User> userList;
+  TextEditingController editingController = TextEditingController();
 
   @override
   void initState() {
@@ -35,49 +36,66 @@ class _PatientChooserState extends State<PatientChooser> {
               color: Colors.lightBlueAccent[700],
             ),
             backgroundColor: Colors.white),
-        body: StreamBuilder<QuerySnapshot>(
-            stream: Firestore.instance
-                .collection("users")
-                //.where("firstName", isGreaterThanOrEqualTo: "")
-                .snapshots(),
-            builder:
-                (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasError)
-                return new Text('Error: ${snapshot.error}');
-              switch (snapshot.connectionState) {
-                case ConnectionState.waiting:
-                  return Center(child: CircularProgressIndicator());
-                default:
-                  var users = List<DocumentSnapshot>();
-                  snapshot.data.documents.forEach((ds) {
-                    if (ds.data["admin"] == null || !ds.data["admin"]) {
-                      users.add(ds);
-                    }
-                  });
-                  // TODO: hide patients that are already on the list
-                  final int eventCount = users.length;
-                  return new ListView.builder(
-                      itemCount: eventCount,
-                      itemBuilder: (context, index) {
-                        final DocumentSnapshot document = users[index];
-                        var fName = document.data["firstName"] ?? "";
-                        var lName = document.data["lastName"] ?? "";
-                        var name = fName + " " + lName;
-                        if (name == " ") {
-                          name = "(No Name) " + document.documentID;
+        body: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+              onChanged: (value) {
+
+              },
+              controller: editingController,
+              decoration: InputDecoration(
+                labelText: "Search",
+                hintText: "Search",
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(25.0)))),
+            )),
+            Expanded(child:StreamBuilder<QuerySnapshot>(
+                stream: Firestore.instance
+                    .collection("users")
+                    //.where("firstName", isGreaterThanOrEqualTo: "")
+                    .snapshots(),
+                builder:
+                    (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError)
+                    return new Text('Error: ${snapshot.error}');
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return Center(child: CircularProgressIndicator());
+                    default:
+                      var users = List<DocumentSnapshot>();
+                      snapshot.data.documents.forEach((ds) {
+                        if (ds.data["admin"] == null || !ds.data["admin"]) {
+                          users.add(ds);
                         }
-                        return new Card(
-                            child: ListTile(
-                            leading: Icon(Icons.face),
-                            title: Text(name),
-                            onTap: () {
-                              print("Add Patient: " + document.documentID);
-                              Firestore.instance.document("users/${appState?.firebaseUserAuth?.uid}").updateData({'patients':FieldValue.arrayUnion([document.documentID])});
-                              Navigator.pop(context);
-                            }
-                        ));
                       });
-              }
-            }));
+                      // TODO: hide patients that are already on the list
+                      final int eventCount = users.length;
+                      return new ListView.builder(
+                          itemCount: eventCount,
+                          itemBuilder: (context, index) {
+                            final DocumentSnapshot document = users[index];
+                            var fName = document.data["firstName"] ?? "";
+                            var lName = document.data["lastName"] ?? "";
+                            var name = fName + " " + lName;
+                            if (name == " ") {
+                              name = "(No Name) " + document.documentID;
+                            }
+                            return new Card(
+                                child: ListTile(
+                                leading: Icon(Icons.face),
+                                title: Text(name),
+                                onTap: () {
+                                  print("Add Patient: " + document.documentID);
+                                  Firestore.instance.document("users/${appState?.firebaseUserAuth?.uid}").updateData({'patients':FieldValue.arrayUnion([document.documentID])});
+                                  Navigator.pop(context);
+                                }
+                            ));
+                          });
+                  }
+                }))])
+    );
   }
 }
