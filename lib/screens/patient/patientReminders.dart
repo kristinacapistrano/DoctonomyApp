af49ea6f129e786f38ced8d0c0bbf1c8876e5eab
Alex A <appdaddy@Alexs-MacBook-Pro.local>
2020-02-28 19:48:30 -0700

    Add App Bar to reminders creen

--- lib/screens/patient/patientReminders.dart
+++ lib/screens/patient/patientReminders.dart
@@ -1,19 +1,25 @@
+import 'package:doctonomy_app/models/state.dart';
+import 'package:doctonomy_app/util/state_widget.dart';
 import 'package:flutter/material.dart';
 
 class PatientReminders extends StatelessWidget {
+  StateModel appState;
+
   @override
   Widget build(BuildContext context) {
+    appState = StateWidget.of(context).state;
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: LinearGradient(
-                colors: [Colors.blue, Colors.blue[50]],
-                begin: Alignment.bottomCenter,
-                end: Alignment.topCenter)),
-        child: SingleChildScrollView(
-          child: Text("Reminder Screen"),
-        ),
-      ),
+      appBar: AppBar(
+          brightness: Brightness.light,
+          textTheme: TextTheme(
+              title: TextStyle(
+            color: Colors.lightBlueAccent[700],
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          )),
+          title: Text("Reminders"),
+          backgroundColor: Colors.white),
+      body: Container(),
     );
   }
 }
