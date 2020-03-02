f2406113537b19fcdba7967940eeaac5104a1e20
Alex A <appdaddy@Alexs-MacBook-Pro.local>
2020-03-01 18:20:36 -0700

    Implement function that fetches reminders for a specific patient

--- lib/screens/patient/patientReminders.dart
+++ lib/screens/patient/patientReminders.dart
@@ -1,9 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:doctonomy_app/models/state.dart';
 import 'package:doctonomy_app/util/state_widget.dart';
 import 'package:flutter/material.dart';
 
 class PatientReminders extends StatelessWidget {
   StateModel appState;
+
+  Future<List<dynamic>> getList() async {
+    DocumentReference docRef = Firestore.instance
+        .collection('reminders')
+        .document(appState?.firebaseUserAuth?.uid ?? "");
+
+    return docRef.get().then((datasnapshot) async {
+      if (datasnapshot.exists) {
+        List<String> list = new List();
+
+        for (final v in datasnapshot.data.values) {
+          list.add(v);
+        }
+
+        return list;
+      } else {
+        return [];
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
